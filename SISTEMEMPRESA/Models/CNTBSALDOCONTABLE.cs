b2e939d6783f3b446e6fc7bf7535d1982f967736
Juan Andrade <jpas.juan01@gmail.com>
2022-03-15 17:09:57 -0500

    Ultimos ajustes, saldos contables y auditorias.

--- SISTEMEMPRESA/Models/CNTBSALDOCONTABLE.cs
+++ SISTEMEMPRESA/Models/CNTBSALDOCONTABLE.cs
@@ -11,16 +11,21 @@ namespace SISTEMEMPRESA.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class CNTBSALDOCONTABLE
     {
         public int IdCntbSaldoContable { get; set; }
         public Nullable<int> IdCntbRegDetalleCont { get; set; }
+        [Display(Name = "Periodo")]
         public short periodo { get; set; }
+        [Display(Name = "Numero Cabecera")]
         public int NumeroLote { get; set; }
+        [Display(Name = "Codigo Cuenta")]
         public int CuentaContable { get; set; }
         public Nullable<decimal> SaldoContableDebe { get; set; }
         public Nullable<decimal> SaldoContableHaber { get; set; }
+        [Display(Name = "Estado")]
         public string estado { get; set; }
     
         public virtual CNTBREGDETALLECONT CNTBREGDETALLECONT { get; set; }
